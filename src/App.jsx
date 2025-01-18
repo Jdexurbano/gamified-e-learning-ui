ce33d055de19aa7dd02ab489577e1c5eb21d5770
Jdexurbano <dexterurbano25@gmail.com>
2025-01-19 02:23:10 +0800

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,37 +11,42 @@ import HomePage from "./pages/student/home/HomePage";
 import AnnouncementPage from "./pages/student/announcement/AnnouncementPage";
 import ActivityHistoryPage from "./pages/student/activity_history/ActivityHistoryPage";
 import CountTheFruitGame from "./pages/games/CountTheFruitGame";
+import AdminAnnouncementPage from "./pages/admin/announcement/AdminAnnouncementPage";
 
 function App() {
-  // return (
-  //   <>
-  //     <Routes>
-  //       {/* admin routes */}
-  //       <Route path="/admin" element={<AdminLogin />} />
-  //       <Route element={<AdminLayout />}>
-  //         <Route path="/admin/home" element={<AdminHome />} />
-  //         <Route path="/admin/student" element={<StudentListPage />} />
-  //       </Route>
-
-  //       {/* student routes */}
-  //       <Route element={<StudentLoginLayout />}>
-  //         <Route path="/" element={<StudentLoginPage />} />
-  //         <Route path="/about" element={<AboutPage />} />
-  //       </Route>
-  //       <Route element={<StudentMainLayout />}>
-  //         <Route path="/home" element={<HomePage />} />
-  //         <Route path="/announcement" element={<AnnouncementPage />} />
-  //         <Route path="/history" element={<ActivityHistoryPage />} />
-  //       </Route>
-
-  //       {/* game routes */}
-  //       <Route path="/count-the-fruits" element={<CountTheFruitGame />} />
-  //     </Routes>
-  //   </>
-  // );
-
-  return(
-    <AdminLogin />
+  return (
+    <>
+      <Routes>
+        {/* admin routes */}
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route element={<AdminLayout />}>
+          <Route path="/admin/home" element={<AdminHome />} />
+          <Route path="/admin/student" element={<StudentListPage />} />
+          <Route
+            path="/admin/announcement"
+            element={<AdminAnnouncementPage />}
+          />
+        </Route>
+        // {/* student routes */}
+        //{" "}
+        <Route element={<StudentLoginLayout />}>
+          // <Route path="/" element={<StudentLoginPage />} />
+          // <Route path="/about" element={<AboutPage />} />
+          //{" "}
+        </Route>
+        //{" "}
+        <Route element={<StudentMainLayout />}>
+          // <Route path="/home" element={<HomePage />} />
+          // <Route path="/announcement" element={<AnnouncementPage />} />
+          // <Route path="/history" element={<ActivityHistoryPage />} />
+          //{" "}
+        </Route>
+        // {/* game routes */}
+        // <Route path="/count-the-fruits" element={<CountTheFruitGame />} />
+        //{" "}
+      </Routes>
+      //{" "}
+    </>
   );
 }
 
