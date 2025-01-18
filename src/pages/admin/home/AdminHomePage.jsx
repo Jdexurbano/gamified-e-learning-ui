661881bd5878b2c9ba500ff19a40175a210d7be4
Jdexurbano <dexterurbano25@gmail.com>
2025-01-19 02:22:44 +0800

    add page info

--- src/pages/admin/home/AdminHomePage.jsx
+++ src/pages/admin/home/AdminHomePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function AdminHome() {
-  return <div>AdminHome</div>;
+  return (
+    <main className="w-full flex justify-center items-center">
+      <img
+        src="../images/page_not_available.png"
+        alt=""
+        className="w-[40rem] h-[35rem]"
+      />
+    </main>
+  );
 }
 
 export default AdminHome;
--- src/pages/student/activity_history/ActivityHistoryPage.jsx
+++ src/pages/student/activity_history/ActivityHistoryPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function ActivityHistoryPage() {
-  return <div>ActivityHistoryPage</div>;
+  return (
+    <main className="w-full flex justify-center items-center">
+      <img
+        src="../images/page_not_available.png"
+        alt=""
+        className="w-[40rem] h-[32rem]"
+      />
+    </main>
+  );
 }
 
 export default ActivityHistoryPage;
